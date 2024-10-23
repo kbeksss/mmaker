a6a4a3832195ab19733f4734bffb66eb5f04ab99
beks <kbeksss@gmail.com>
2024-10-23 22:46:44 +0600

    exchanges

--- src/sections/account/account-exchange.jsx
+++ src/sections/account/account-exchange.jsx
@@ -1,5 +1,29 @@
 import { Grid, Card } from '@mui/material';
 
+const exchangeList = [
+  { name: 'Binance', value: 'binance', icon: '/assets/images/exchange/binance.png' },
+  {
+    name: 'Coinbase exchange',
+    value: 'coinbase',
+    icon: '/assets/images/exchange/coinbase.webp',
+  },
+  {
+    name: 'Bybit',
+    value: 'bybit',
+    icon: '/assets/images/exchange/bybit.png',
+  },
+  {
+    name: 'OKX',
+    value: 'okx',
+    icon: '/assets/images/exchange/okx.png',
+  },
+  {
+    name: 'Upbit',
+    value: 'upbit',
+    icon: '/assets/images/exchange/upbit.png',
+  },
+];
+
 export function AccountExchange() {
   return (
     <Grid container spacing={3}>
@@ -9,7 +33,12 @@ export function AccountExchange() {
             p: 3,
           }}
         >
-          exchange
+          {exchangeList.map((exch) => (
+            <div key={exch.value}>
+              <img src={exch.icon} alt="" />
+              {exch.name}
+            </div>
+          ))}
         </Card>
       </Grid>
       <Grid xs={12} md={8} item>
