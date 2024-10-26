db7215e925ca86ffc248cb2f5eea2ec7e2d11454
beks <kbeksss@gmail.com>
2024-10-26 23:37:25 +0600

    connected exchanges

---
+++ src/sections/account/account-exchange-list.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import {
+  Box,
+  Card,
+  Table,
+  Avatar,
+  TableRow,
+  TableCell,
+  TableBody,
+  CardHeader,
+} from '@mui/material';
+
+import { Scrollbar } from 'src/components/scrollbar';
+import { TableHeadCustom } from 'src/components/table';
+import { fCurrency } from 'src/utils/format-number';
+import { Label } from 'src/components/label';
+
+export const AccountExchangeList = ({ title, subheader, tableData, headLabel, ...other }) => (
+  <Card {...other}>
+    <CardHeader title={title} subheader={subheader} sx={{ mb: 3 }} />
+
+    <Scrollbar>
+      <Table sx={{ minWidth: 640 }}>
+        <TableHeadCustom headLabel={headLabel} />
+        <TableBody>
+          {tableData.map((row) => (
+            <RowItem key={row.id} row={row} />
+          ))}
+        </TableBody>
+      </Table>
+    </Scrollbar>
+  </Card>
+);
+
+function RowItem({ row }) {
+  return (
+    <TableRow>
+      <TableCell>
+        <Box sx={{ gap: 2, display: 'flex', alignItems: 'center' }}>
+          <Avatar
+            variant="square"
+            sx={{ height: 'auto' }}
+            alt={row.exchangeName}
+            src={row.iconUrl}
+          />
+          {row.exchangeName}
+        </Box>
+      </TableCell>
+
+      <TableCell>{row.date}</TableCell>
+
+      <TableCell align="center">{fCurrency(row.balance)}</TableCell>
+
+      <TableCell align="right">
+        <Label
+          variant="soft"
+          color={
+            (row.status === 'active' && 'primary') ||
+            (row.status === 'paused' && 'secondary') ||
+            'error'
+          }
+        >
+          {row.status}
+        </Label>
+      </TableCell>
+    </TableRow>
+  );
+}
--- src/sections/account/account-exchange.jsx
+++ src/sections/account/account-exchange.jsx
@@ -1,17 +1,22 @@
 import { useState } from 'react';
 import { Grid, Card, Stack, Typography, Box } from '@mui/material';
 import Lottie from 'react-lottie-player';
-import whaleLottie from 'src/assets/lotties/whale.json';
-import { Field, Form } from 'src/components/hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { z as zod } from 'zod';
 import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
+
+import whaleLottie from 'src/assets/lotties/whale.json';
+import { Field, Form } from 'src/components/hook-form';
+import { _mock } from 'src/_mock';
 import ExchangeCard from './exchange-card';
+import { AccountExchangeList } from './account-exchange-list';
 
 export const KeysSchema = zod.object({
   apiKey: zod.string().min(1, { message: 'Api key is required!' }),
   secretKey: zod.string().min(1, { message: 'Secret key is required!' }),
 });
+
+export const StatusType = { active: 'active', paused: 'paused' };
 
 const exchangeList = [
   { name: 'Binance', value: 'binance', icon: '/assets/images/exchange/binance.png' },
@@ -37,6 +42,18 @@ const exchangeList = [
   },
 ];
 
+export const tempData = [...Array(5)].map((_, index) => {
+  const exchangeName = ['Binance', 'Coinbase exchange', 'Bybit', 'OKX', 'Upbit'][index];
+  return {
+    id: _mock.id(index),
+    exchangeName,
+    date: '19 oct. 2024',
+    balance: _mock.number.price(index),
+    status: StatusType.active,
+    iconUrl: exchangeList[index].icon,
+  };
+});
+
 export function AccountExchange() {
   const [selectedExchange, setSelectedExchange] = useState(null);
   const methods = useForm({
@@ -52,58 +69,72 @@ export function AccountExchange() {
     console.log('data', data);
   });
   return (
-    <Grid container spacing={3}>
-      <Grid xs={12} md={4} item>
-        <Typography sx={{ mb: 2 }}>Choose exchange</Typography>
-        <Stack spacing={1}>
-          {exchangeList.map((exch) => (
-            <ExchangeCard
-              key={exch.value}
-              onCardClick={() => setSelectedExchange(exch)}
-              selected={selectedExchange?.value === exch.value}
-              name={exch.name}
-              img={exch.icon}
-            />
-          ))}
-        </Stack>
-      </Grid>
-      <Grid xs={12} md={8} item>
-        <Card
-          sx={{
-            p: 3,
-          }}
-        >
-          {!selectedExchange ? (
-            <>
-              <Typography>Choose exchange</Typography>
-              <Lottie loop animationData={whaleLottie} play style={{ height: 300 }} />
-            </>
-          ) : (
-            <Box>
-              <Grid container justifyContent="center" spacing={3} alignItems="center">
-                <Grid item>
-                  <img width={150} src={selectedExchange.icon} alt={selectedExchange.name} />
-                </Grid>
-                <Grid item>
-                  <Typography variant="h2">{selectedExchange.name}</Typography>
-                </Grid>
-              </Grid>
-              <Box sx={{ pt: 4 }}>
-                <Form methods={methods} onSubmit={onSubmit}>
-                  <Grid container spacing={2}>
-                    <Grid item xs={12} sm={6}>
-                      <Field.Text label="Api key" name="apiKey" />
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                      <Field.Text label="Secret key" name="secretKey" />
-                    </Grid>
+    <>
+      <Box sx={{ pb: 2 }}>
+        <AccountExchangeList
+          title="Connected Exchanges"
+          tableData={tempData}
+          headLabel={[
+            { id: 'exchangeName', label: 'Exchange' },
+            { id: 'date', label: 'Date of connection' },
+            { id: 'balance', label: 'Balance', align: 'center' },
+            { id: 'status', label: 'Status', align: 'right' },
+          ]}
+        />
+      </Box>
+      <Grid container spacing={3}>
+        <Grid xs={12} md={4} item>
+          <Typography sx={{ mb: 2 }}>Choose exchange</Typography>
+          <Stack spacing={1}>
+            {exchangeList.map((exch) => (
+              <ExchangeCard
+                key={exch.value}
+                onCardClick={() => setSelectedExchange(exch)}
+                selected={selectedExchange?.value === exch.value}
+                name={exch.name}
+                img={exch.icon}
+              />
+            ))}
+          </Stack>
+        </Grid>
+        <Grid xs={12} md={8} item>
+          <Card
+            sx={{
+              p: 3,
+            }}
+          >
+            {!selectedExchange ? (
+              <>
+                <Typography>Choose exchange</Typography>
+                <Lottie loop animationData={whaleLottie} play style={{ height: 300 }} />
+              </>
+            ) : (
+              <Box>
+                <Grid container justifyContent="center" spacing={3} alignItems="center">
+                  <Grid item>
+                    <img width={150} src={selectedExchange.icon} alt={selectedExchange.name} />
                   </Grid>
-                </Form>
+                  <Grid item>
+                    <Typography variant="h2">{selectedExchange.name}</Typography>
+                  </Grid>
+                </Grid>
+                <Box sx={{ pt: 4 }}>
+                  <Form methods={methods} onSubmit={onSubmit}>
+                    <Grid container spacing={2}>
+                      <Grid item xs={12} sm={6}>
+                        <Field.Text label="Api key" name="apiKey" />
+                      </Grid>
+                      <Grid item xs={12} sm={6}>
+                        <Field.Text label="Secret key" name="secretKey" />
+                      </Grid>
+                    </Grid>
+                  </Form>
+                </Box>
               </Box>
-            </Box>
-          )}
-        </Card>
+            )}
+          </Card>
+        </Grid>
       </Grid>
-    </Grid>
+    </>
   );
 }
